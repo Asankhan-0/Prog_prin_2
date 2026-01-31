b5944d55613c2724c34c8049d752174637d10350
Asankhan-0 <asankhan030308@gmail.com>
2026-01-31 11:05:15 +0500

    Strings.py
    Removed unused string operations and examples.

--- Lab1/Strings.py
+++ Lab1/Strings.py
@@ -17,35 +17,13 @@ print(a[1])  # e
 b = "Hello, World!"
 print(b[2:5])  # llo
 
-# Negative Indexing
-c = "Hello, World!"
-print(c[-5:-2])  # orl
-
 # String Length
 d = "Hello, World!"
 print(len(d))  # 13
 
-# String Methods
-e = "hello, world!"
-
 # Check string
 txt = "The best things in life are free!"
 print("free" in txt) # True
-
-# Check if not
-txt = "The best things in life are free!"
-print("expensive" not in txt) # True
-
-#Slicing Strings
-b = "Hello, World!"
-print(b[2:5])  # llo
-print(b[:5])   # Hello
-
-#Negative Indexing
-c = "Hello, World!"
-print(c[-5:-2])  # orl
-print(c[-5:])    # orld!
-print(c[:-2])    # Hello, Worl
 
 #Uppercase
 f = "hello, world!"
@@ -55,33 +33,11 @@ print(f.upper())  # HELLO, WORLD!
 g = "HELLO, WORLD!"
 print(g.lower())  # hello, world!
 
-# Remove Whitespace
-h = "   Hello, World!   "
-print(h.strip())  # "Hello, World!"
-
-#Split String
-i = "Hello, World!"
-print(i.split(","))  # ['Hello', ' World!']
-
 #String Concatenation
 j = "Hello"
 k = "World"
 print(j + " " + k)  # Hello World
 
-#String Format
-name = "John"
-age = 36
-txt = "My name is {}, and I am {}"
-print(txt.format(name, age))  # My name is John, and I am 36
-
-#Placeholder
-quantity = 3
-itemno = 5
-price = 50.00
-myorder = "
-I want {} pieces of item {} for {} dollars."
-print(myorder.format(quantity, itemno, price))
-# I want 3 pieces of item 5 for 50.00 dollars.
 
 #Escape Characters
 txt = "We are the so-called \"Vikings\" from the north."
